c13e78d7a2bd10d3064f150665d5b69a4a020afa
oldsalao <bucktoothvillain@gmail.com>
2021-10-30 16:41:45 +0900

    Feat : Add MapBox

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,13 @@
+import styled from "styled-components";
+
+const MapBox = styled.div`
+  border: 1px solid black;
+  width: 1024px;
+  height: 768px;
+`;
+
 const Map = () => {
-  return <div></div>;
+  return <MapBox />;
 };
 
 export default Map;
